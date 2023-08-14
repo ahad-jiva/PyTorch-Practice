83139742608bd6ef4f26d26ce4c99d2fc9e46c14
ahadjiva <ajiva@calpoly.edu>
2023-08-14 13:48:51 -0700

    updated functions

--- temperature converter/temp_convert.py
+++ temperature converter/temp_convert.py
@@ -6,35 +6,35 @@ if users_unit == ('fahrenheit' or 'Fahrenheit'):
     if target_unit == ('celsius' or 'Celsius'):
         input_temp = get_input_temp(users_unit)
         output_temp = far_to_cel(input_temp)
-        print(f'That is {output_temp} degrees celsius')
+        print_temp(output_temp, target_unit)
     elif target_unit == ('kelvin' or 'Kelvin'):
         input_temp = get_input_temp(users_unit)
         output_temp = far_to_kelv(input_temp)
-        print(f'That is {output_temp} degrees Kelvin')
+        print_temp(output_temp, target_unit)
 elif users_unit == ('celsius' or 'Celsius'):
     target_unit = get_target_unit()
     if target_unit == ('fahrenheit' or 'Fahrenheit'):
         input_temp = get_input_temp(users_unit)
         output_temp = cel_to_far(input_temp)
-        print(f'That is {output_temp} degrees fahrenheit')
+        print_temp(output_temp, target_unit)
     elif target_unit == ('kelvin' or 'Kelvin'):
         input_temp = get_input_temp(users_unit)
         output_temp = cel_to_kelv(input_temp)
-        print(f'That is {output_temp} degrees Kelvin')
+        print_temp(output_temp, target_unit)
 elif users_unit == ('kelvin' or 'Kelvin'):
     target_unit = get_target_unit()
     if target_unit == ('fahrenheit' or 'Fahrenheit'):
         input_temp = get_input_temp(users_unit)
         if float(input_temp) >= 0:
             output_temp = kelv_to_far(input_temp)
-            print(f'That is {output_temp} degrees fahrenheit')
+            print_temp(output_temp, target_unit)
         else:
             print('You cannot have less than zero kelvin')
     elif target_unit == ('celsius' or 'Celsius'):
         input_temp = get_input_temp(users_unit)
         if float(input_temp) >= 0:
             output_temp = kelv_to_cel(input_temp)
-            print(f'That is {output_temp} degrees celsius')
+            print_temp(output_temp, target_unit)
         else:
             print('You cannot have less than zero kelvin')
 else:
